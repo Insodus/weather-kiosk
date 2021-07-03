dc89691a1f18dbba4f2728567c3d1eefac6be625
Insodus <insodus@gmail.com>
2021-07-03 16:04:55 -0400

    Adding scripts for weather and water temp

---
+++ scripts/get_forecast.js
@@ -0,0 +1,55 @@
+var request = require('request');
+
+var opts = {
+    url: 'https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=01036:US&units=e&language=en&format=json&apiKey=c9b9de42f3d44b64b9de42f3d4db64db',
+    timeout: 10000
+};
+
+request(opts, function (err, res, body) {
+    if (err) {
+        console.error('Could not get forecast from API!', err);
+        return
+    }
+
+    var weather = JSON.parse(body);
+    var detail = weather.daypart[0];
+
+    console.log(weather.temperatureMax);
+    console.log(weather.temperatureMin);
+    console.log(detail.daypartName);
+    console.log('----');
+
+    var forecastsAdded = 0;
+    var totalNeeded = 4;
+    var currentIndex = 0;
+    var forecastsAvailable = weather.temperatureMin.length;
+
+    while(forecastsAdded < totalNeeded && currentIndex < forecastsAvailable) {
+        var dayIndex = currentIndex * 2;
+        var nightIndex = dayIndex + 1;
+
+        var day = weather.dayOfWeek[currentIndex];
+        var maxTemp = weather.temperatureMax[currentIndex];
+        var minTemp = weather.temperatureMin[currentIndex];
+
+        var dayName = detail.daypartName[dayIndex];
+        var detailDay = detail.narrative[dayIndex];
+
+        var nightName = detail.daypartName[nightIndex];
+        var nightDay = detail.narrative[nightIndex];
+
+        console.log('Date:', day, 'Min:', minTemp, 'Max:', maxTemp);
+
+        if (weather.temperatureMax[currentIndex] != null && detail.narrative[dayIndex] != null) {
+            // daytime isnt over yet so add this one
+            console.log(dayName, detailDay);
+
+            forecastsAdded++;
+        }
+
+        console.log(nightName, nightDay);
+        forecastsAdded++;
+        currentIndex++;
+    }
+
+});
---
+++ scripts/get_water_temp.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var fs = require('fs');
+
+var opts = {
+    url: 'https://lakemonster.com/lake/NH/Lake%20Sunapee-water-temperature-3141',
+    timeout: 10000
+};
+
+var stringMatcher = /<span class='water_text' id='water_text'>([0-9]+)°<\/span>/;
+
+request(opts, function (err, res, body) {
+    if (err) {
+        console.error('Could not get water temp webpage!', err);
+        return
+    }
+
+    var statusCode = res.statusCode;
+    var match = stringMatcher.exec(body);
+    if (match != null) {
+        console.log('Its ', match[1], 'degrees');
+    } else {
+        console.error('Could not find water temp on page!');
+    }
+});
