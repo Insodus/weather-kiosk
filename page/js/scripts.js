95d0dd60c5f98565f89c8d415f27aabda88f2420
Insodus <insodus@gmail.com>
2021-07-05 09:56:51 -0400

    Adding click to zoom

--- page/js/scripts.js
+++ page/js/scripts.js
@@ -3,5 +3,15 @@ setTimeout(() => {
 }, 30000);
 
 $(function () {
-    $('[data-toggle="popover"]').popover()
-  })
+    $('[data-toggle="popover"]').popover();
+
+    $("#radar-wrap").on('click', function () {
+        if ($("#radar-wrap").hasClass("radar-close")) {
+            $("#radar-wrap").removeClass("radar-close");
+            $("#radar-wrap").addClass("radar-wide");
+        } else {
+            $("#radar-wrap").addClass("radar-close");
+            $("#radar-wrap").removeClass("radar-wide");
+        }
+    });
+});
