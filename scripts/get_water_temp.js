8a18e78e5bffd529b3048af6951f5d10c4ebcaab
Insodus <insodus@gmail.com>
2021-07-04 22:00:19 -0400

    Updating water temp

--- scripts/get_water_temp.js
+++ scripts/get_water_temp.js
@@ -1,10 +1,11 @@
 var request = require('request');
 var fs = require('fs');
+var app_config = require('./app_config');
 
 function getWaterTemp() {
     return new Promise((resolve, reject) => {
         var opts = {
-            url: 'https://lakemonster.com/lake/NH/Lake%20Sunapee-water-temperature-3141',
+            url: `https://lakemonster.com/lake/${app_config.lakemonster_url}`,
             timeout: 10000
         };
         var stringMatcher = /<span class='water_text' id='water_text'>([0-9]+)°<\/span>/;
