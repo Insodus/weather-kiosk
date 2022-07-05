a72d5842e6af0425dffff2a91a4f80f09c055422
Insodus <insodus@gmail.com>
2022-07-05 08:45:27 -0400

    New lakemonster parse

--- scripts/get_water_temp.js
+++ scripts/get_water_temp.js
@@ -6,10 +6,9 @@ var app_config = require('./app_config');
 function getWaterTemp() {
     return new Promise((resolve, reject) => {
         var opts = {
-            url: `https://lakemonster.com/lake/${app_config.lakemonster_url}`,
+            url: app_config.lakemonster_url,
             timeout: 10000
         };
-        var stringMatcher = /<span class='water_text' id='water_text'>([0-9]+)°<\/span>/;
 
         request(opts, function (err, res, body) {
             if (err) {
@@ -17,9 +16,9 @@ function getWaterTemp() {
                 return;
             }
 
-            var match = stringMatcher.exec(body);
+            var match = app_config.lakemonster_matcher.exec(body);
             if (match != null) {
-                resolve(match[1]);
+                resolve(match[1].trim());
             } else {
                 reject('Could not find water temp on page!');
             }
@@ -29,8 +28,10 @@ function getWaterTemp() {
 
 async function getAndStore() {
     var water_temp = await getWaterTemp();
-    var last_update = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    var output_json = ` { "water_temp" : ${water_temp} , "last_update" : "${last_update}" } `;
+    var curr_time = moment();
+    var last_update = curr_time.format("dddd, MMMM Do YYYY, h:mm:ss a");
+    var last_update_ts = curr_time.unix();
+    var output_json = ` { "water_temp" : "${water_temp}" , "water_update" : "${last_update}" , "water_update_ts" : ${last_update_ts} } `;
     fs.writeFile('cache_water_temp.json', output_json, 'utf8', function (err) {
         if (err) throw err;
         console.log('Wrote water temp.');
